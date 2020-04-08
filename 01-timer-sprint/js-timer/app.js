bdfb4a1846b553d2305e6192de242b6f7f7430a6
eessgg <estergomesdesign@gmail.com>
2020-04-08 18:50:35 -0300

    time to be completed

--- 01-timer-sprint/js-timer/app.js
+++ 01-timer-sprint/js-timer/app.js
@@ -1,65 +1,42 @@
-var countDownDate = new Date().getFullYear();
-const calendar = document.getElementById("calendar");
-const today = document.querySelector(".today");
-const hours = document.querySelector(".hours");
-const minutes = document.querySelector(".minutes");
-const seconds = document.querySelector(".seconds");
+  const calendar = document.querySelector('.calendar');
+
+    function getTime(time) {
+      //calendar
+      let endtime = new Date(time).getTime();
+
+      //setInterval
+      var countdown = setInterval(function () {
+        let now = new Date().getTime(); // time now
+        let distanceToEnd = endtime - now; // distance now / end
+
+        // Calc hours,secs,mins
+        let hours = Math.floor((distanceToEnd % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+        let minutes = Math.floor((distanceToEnd % (1000 * 60 * 60)) / (1000 * 60));
+        let seconds = Math.floor((distanceToEnd % (1000 * 60)) / 1000);
+
+        let clock = {
+          hours: hours,
+          minutes: minutes,
+          seconds: seconds
+        }
+        displayClock(clock)
+
+      }, 1000)
+    }
 
 
-
-calendar.addEventListener("change", function () {
-  var dateinput = this.value;
-  var timecalendar = new Date(dateinput);
-  // dates ↓
-  const timer = {
-    day: timecalendar.getDate(),
-    hour: timecalendar.getHours(),
-    mins: timecalendar.getMinutes(),
-    secs: timecalendar.getSeconds(),
-  };
-  createClock(timer);
-});
+    function displayClock(clock) {
+      let clockDivHour = document.querySelector('.clock-hour')
+      let clockDivMins = document.querySelector('.clock-mins')
+      let clockDivSecs = document.querySelector('.clock-secs')
 
 
-function createClock(timer) {
-
-  console.log(timer.day)
-  today.innerHTML = timer.day + ' de ' + "";
-  hours.textContent = timer.hour + 1;
-
-  if(timer.mins === 0) {
-    minutes.textContent = "0" + timer.mins;
-  }
-
-  if (timer.seconds === 0) {
-    seconds.textContent = "0" + timer.secs;
-  }
-}
-
-
-
-
-
-
-
-
-
-
-
-//  setInterval(createTimer, 1000);
-
-// function myDate () {
-//   var currentDate = new Date();
-
-//   var date = currentDate.getDate();
-//   var hour = currentDate.getHours();
-//   var mins = currentDate.getMinutes();
-//   var secs = currentDate.getSeconds();
-//   var timestamp = currentDate.getTime();
-
-//   var dateString = `Dia: ${date} - Hora: ${hour} - Minutes: ${mins} - Seconds: ${secs} -
-//     timestamp ${timestamp}`;
-
-//   console.log(dateString)
-// }
-// myDate()
+      if(clock.hours < 10) {
+        clockDivHour.innerHTML = '0' + clock.hours;
+      }
+      if(clock.minutes < 10) {
+        clockDivMins.innerHTML = '0' + clock.minutes;
+      }
+      if(clock.seconds < 10) { clockDivMins.innerHTML='0' + clock.seconds; }
+      
+    }
